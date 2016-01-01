50760ae2e323a4471b66caf13df5a0e130d08748
Stefan Urbanek <stefan@agentfarms.net>
2016-01-01 12:11:12 -0800

    Initial Commit

---
+++ ParserCombinator/ParserCombinator.h
@@ -0,0 +1,19 @@
+//
+//  ParserCombinator.h
+//  ParserCombinator
+//
+//  Created by Stefan Urbanek on 01/01/16.
+//  Copyright © 2016 Stefan Urbanek. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ParserCombinator.
+FOUNDATION_EXPORT double ParserCombinatorVersionNumber;
+
+//! Project version string for ParserCombinator.
+FOUNDATION_EXPORT const unsigned char ParserCombinatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParserCombinator/PublicHeader.h>
+
+
